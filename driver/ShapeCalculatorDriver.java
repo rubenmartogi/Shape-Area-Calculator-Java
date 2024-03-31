2b0782f9dc2801c35a6bf067f380d910c8c079b7
Samuel Sihotang <samsihotang1@gmail.com>
2024-03-31 23:02:23 +0700

    update use enum

--- driver/ShapeCalculatorDriver.java
+++ driver/ShapeCalculatorDriver.java
@@ -1,16 +1,50 @@
 package driver;
 
-import model.*;
+import java.util.Scanner;
+
+import model.Circle;
+import model.Rectangle;
+import model.Shape;
+import model.ShapeType;
+import model.Triangle;
 
 public class ShapeCalculatorDriver {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         Circle circle = new Circle(5);
         Rectangle rectangle = new Rectangle(4, 6);
         Triangle triangle = new Triangle(3, 4);
 
-        System.out.println("Luas Lingkaran: " + calculateArea(circle));
-        System.out.println("Luas Persegi: " + calculateArea(rectangle));
-        System.out.println("Luas Segitiga: " + calculateArea(triangle));
+        System.out.println("Pilih jenis bentuk (0: Lingkaran, 1: Persegi Panjang, 2: Segitiga): ");
+        int choice = scanner.nextInt();
+        ShapeType shapeType = null;
+
+        switch (choice) {
+            case 0:
+                shapeType = ShapeType.CIRCLE;
+                break;
+            case 1:
+                shapeType = ShapeType.RECTANGLE;
+                break;
+            case 2:
+                shapeType = ShapeType.TRIANGLE;
+                break;
+            default:
+                System.out.println("Pilihan tidak valid!");
+        }
+
+        if (shapeType == ShapeType.CIRCLE) {
+            System.out.println("Luas Lingkaran: " + calculateArea(circle));
+        } else if (shapeType == ShapeType.RECTANGLE) {
+            System.out.println("Luas Persegi Panjang: " + calculateArea(rectangle));
+        } else if (shapeType == ShapeType.TRIANGLE) {
+            System.out.println("Luas Segitiga: " + calculateArea(triangle));
+        } else {
+            System.out.println("Pilihan tidak valid!");
+        }
+
+        scanner.close();
     }
 
     private static double calculateArea(Shape shape) {
